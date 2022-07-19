7f759cf640f2e0e11d4aedcba173809bb305ffb4
Rômulo Fernandes Evangelista <romfe89@gmail.com>
2022-07-19 09:22:14 -0300

    posts being correctly loaded into the frontend

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="bg-gray-900 text-gray-100 shadow w-full">
+    <header className="bg-gray-900 text-gray-100 shadow w-full font-jetbrains">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <Link href="/">
           <a className="flex md:w-1/5 title-font font-medium items-center md:justify-start mb-4 md:mb-0">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,44 @@
 import type { NextPage } from "next";
+import matter from "gray-matter";
 import fs from "fs";
 import path from "path";
 import Layout from "../components/Layout";
 
-const HomePage: NextPage = () => {
+interface HomePageProps {
+  posts: {
+    title: string;
+    date: string;
+    excerpt: string;
+    cover_image: string;
+    category: string;
+    author: string;
+    author_image: string;
+  }[];
+}
+
+const HomePage = ({ posts }: HomePageProps) => {
+  console.log(posts);
   return (
     <Layout>
-      <h1>Hello WAAAARUDOOOOOOOO!</h1>
+      <h1>Últimos Posts</h1>
     </Layout>
   );
 };
 
 export default HomePage;
 export async function getStaticProps() {
-  const files = fs.readdirSync(path.join("posts"));
-  console.log(files);
+  const files = fs.readdirSync(path.join("src/posts"));
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+    const markdownWithMeta = fs.readFileSync(
+      path.join("src/posts", filename),
+      "utf-8"
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+    return { slug, frontmatter };
+  });
   return {
-    props: {},
+    props: { posts },
   };
 }
