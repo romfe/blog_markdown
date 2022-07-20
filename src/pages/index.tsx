66cc37cab2a8edc39b2edaaea1a4de53c42dca22
Rômulo Fernandes Evangelista <romfe89@gmail.com>
2022-07-19 22:39:03 -0300

    post miniatures added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,49 @@
-import type { NextPage } from "next";
+//import type { NextPage } from "next";
 import matter from "gray-matter";
 import fs from "fs";
 import path from "path";
+import Link from "next/link";
 import Layout from "../components/Layout";
+import Post from "../components/Post";
 
 interface HomePageProps {
   posts: {
-    title: string;
-    date: string;
-    excerpt: string;
-    cover_image: string;
-    category: string;
-    author: string;
-    author_image: string;
+    frontmatter: {
+      title: string;
+      date: string;
+      excerpt: string;
+      cover_image: string;
+      category: string;
+      author: string;
+      author_image: string;
+    };
+    slug: string;
   }[];
 }
 
 const HomePage = ({ posts }: HomePageProps) => {
-  console.log(posts);
   return (
     <Layout>
-      <h1>Últimos Posts</h1>
+      <h1 className="text-5xl border-b4 p-5 font-bold">Últimos Posts</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+      <Link href="/blog">
+        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
+          Todos os posts
+        </a>
+      </Link>
     </Layout>
   );
 };
 
 export default HomePage;
+
 export async function getStaticProps() {
   const files = fs.readdirSync(path.join("src/posts"));
+
   const posts = files.map((filename) => {
     const slug = filename.replace(".md", "");
     const markdownWithMeta = fs.readFileSync(
@@ -38,6 +54,7 @@ export async function getStaticProps() {
     const { data: frontmatter } = matter(markdownWithMeta);
     return { slug, frontmatter };
   });
+
   return {
     props: { posts },
   };
