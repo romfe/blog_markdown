06fbc06a8a1d200cf148253351a390d0360bd8f0
Rômulo Fernandes Evangelista <romfe89@gmail.com>
2022-07-21 22:38:12 -0300

    some code refactoring

--- src/components/CategoryLabel.tsx
+++ src/components/CategoryLabel.tsx
@@ -6,18 +6,17 @@ interface CategoryLabelProps {
 }
 
 const CategoryLabel = ({ children }: CategoryLabelProps) => {
-  const colorKey = {
+  const colorKey: { [key: string]: string } = {
     JavaScript: "yellow",
     CSS: "blue",
     Python: "green",
     PHP: "purple",
     Ruby: "red",
   };
-  const color = colorKey[children as keyof typeof colorKey];
   return (
     <div
-      //className={`px-2 py-1 bg-${color}-600 text-gray-100 text-bold rounded`}
-      className={styles[color]}
+      //className={`px-2 py-1 bg-${colorKey[children]}-600 text-gray-100 text-bold rounded`}
+      className={styles[colorKey[children]]}
     >
       <Link href={`/blog/category/${children.toLowerCase()}`}>{children}</Link>
     </div>
